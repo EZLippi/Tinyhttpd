b9158f30ed8c94190ed4b6f7f0bbcbba405765a8
qian <nancy.wang@neulion.com.cn>
2022-05-13 17:50:29 +0800

    Add comment.

--- httpd.c
+++ httpd.c
@@ -12,6 +12,9 @@
  *  4) Uncomment the line that runs accept_request().
  *  5) Remove -lsocket from the Makefile.
  */
+
+//ref: https://zhuanlan.zhihu.com/p/24941375
+
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -206,8 +209,8 @@ void execute_cgi(int client, const char *path,
                  const char *method, const char *query_string)
 {
  char buf[1024];
- int cgi_output[2];
- int cgi_input[2];
+ int cgi_output[2];//output pipe, 0 read, 1 write
+ int cgi_input[2];//input pipe, 0 read, 1 write
  pid_t pid;
  int status;
  int i;
@@ -221,18 +224,18 @@ void execute_cgi(int client, const char *path,
    numchars = get_line(client, buf, sizeof(buf));
  else    /* POST */
  {
-  numchars = get_line(client, buf, sizeof(buf));
-  while ((numchars > 0) && strcmp("\n", buf))
-  {
-   buf[15] = '\0';
-   if (strcasecmp(buf, "Content-Length:") == 0)
-    content_length = atoi(&(buf[16]));
-   numchars = get_line(client, buf, sizeof(buf));
-  }
-  if (content_length == -1) {
-   bad_request(client);
-   return;
-  }
+      numchars = get_line(client, buf, sizeof(buf));
+      while ((numchars > 0) && strcmp("\n", buf))
+      {
+       buf[15] = '\0';
+       if (strcasecmp(buf, "Content-Length:") == 0)
+        content_length = atoi(&(buf[16]));
+       numchars = get_line(client, buf, sizeof(buf));
+      }
+      if (content_length == -1) {
+       bad_request(client);
+       return;
+      }
  }
 
  sprintf(buf, "HTTP/1.0 200 OK\r\n");
@@ -253,40 +256,43 @@ void execute_cgi(int client, const char *path,
  }
  if (pid == 0)  /* child: CGI script */
  {
-  char meth_env[255];
-  char query_env[255];
-  char length_env[255];
+      char meth_env[255];
+      char query_env[255];
+      char length_env[255];
+      dup2(cgi_output[1], 1);//1代表着stdout，0代表着stdin，将系统标准输出重定向为cgi_output[1]
+      dup2(cgi_input[0], 0);//将系统标准输入重定向为cgi_input[0]，
+      close(cgi_output[0]);
+      close(cgi_input[1]);
+      sprintf(meth_env, "REQUEST_METHOD=%s", method);
+      putenv(meth_env);
+      if (strcasecmp(method, "GET") == 0) 
+      {
+       sprintf(query_env, "QUERY_STRING=%s", query_string);
+       putenv(query_env);
+      }
+      else 
+      {   /* POST */
+       sprintf(length_env, "CONTENT_LENGTH=%d", content_length);
+       putenv(length_env);
+      }
+      execl(path, path, NULL);
+      exit(0);
+ } 
+ else 
+ {    /* parent */
+      close(cgi_output[1]);
+      close(cgi_input[0]);
+      if (strcasecmp(method, "POST") == 0)
+       for (i = 0; i < content_length; i++) {
+        recv(client, &c, 1, 0);
+        write(cgi_input[1], &c, 1);
+       }
+      while (read(cgi_output[0], &c, 1) > 0)
+       send(client, &c, 1, 0);
 
-  dup2(cgi_output[1], 1);
-  dup2(cgi_input[0], 0);
-  close(cgi_output[0]);
-  close(cgi_input[1]);
-  sprintf(meth_env, "REQUEST_METHOD=%s", method);
-  putenv(meth_env);
-  if (strcasecmp(method, "GET") == 0) {
-   sprintf(query_env, "QUERY_STRING=%s", query_string);
-   putenv(query_env);
-  }
-  else {   /* POST */
-   sprintf(length_env, "CONTENT_LENGTH=%d", content_length);
-   putenv(length_env);
-  }
-  execl(path, path, NULL);
-  exit(0);
- } else {    /* parent */
-  close(cgi_output[1]);
-  close(cgi_input[0]);
-  if (strcasecmp(method, "POST") == 0)
-   for (i = 0; i < content_length; i++) {
-    recv(client, &c, 1, 0);
-    write(cgi_input[1], &c, 1);
-   }
-  while (read(cgi_output[0], &c, 1) > 0)
-   send(client, &c, 1, 0);
-
-  close(cgi_output[0]);
-  close(cgi_input[1]);
-  waitpid(pid, &status, 0);
+      close(cgi_output[0]);
+      close(cgi_input[1]);
+      waitpid(pid, &status, 0);
  }
 }
 
